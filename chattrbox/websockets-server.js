3bc51094439385e41bf47d75b00f24dfece9690b
Joesh Bautista <joeshbautista@csu.fullerton.edu>
2019-10-14 14:52:07 -0700

    added '/topic' command to websockets-server.js

--- chattrbox/websockets-server.js
+++ chattrbox/websockets-server.js
@@ -17,9 +17,19 @@ ws.on('connection', function(socket) {
 
   socket.on('message', function(data) {
     console.log('message received: ' + data);
-    messages.push(data);
-    ws.clients.forEach(function(clientSocket) {
-      clientSocket.send(data)
-    });
+    //'/topic' command begin - extracts the first 6 characters of the message to see if it is a command (i.e /topic)
+    if (data.substring(0, 6) === '/topic') {
+      ws.clients.forEach(function(clientSocket) {
+        //Sends to client: '*** Topic has changed to [new topic]'
+        clientSocket.send('*** Topic has changed to \'' + data.substring(7) + '\'')
+      });
+      //Place the topic at the beginning of the messages array
+      messages.unshift('*** Topic is \'' + data.substring(7) + '\'');
+    } else {
+      messages.push(data);
+      ws.clients.forEach(function(clientSocket) {
+        clientSocket.send(data)
+      });
+    }
   });
 });
